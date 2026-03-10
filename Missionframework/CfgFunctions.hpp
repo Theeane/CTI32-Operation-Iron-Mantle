316ac19b569c3aa2f08813f4e923cba49063f7a2
Theeane <envall.kim@gmail.com>
2026-03-10 20:25:05 +0100

    Update CfgFunctions.hpp

--- Missionframework/CfgFunctions.hpp
+++ Missionframework/CfgFunctions.hpp
@@ -3,22 +3,21 @@ class CfgFunctions {
         tag = "AGS";
         class Core {
             file = "Missionframework\core";
-            class initGlobals { recompile = 1; };
-            class scanZones { recompile = 1; };
-            class economy { recompile = 1; };
-            class zoneCapture { recompile = 1; };
-            class notorietyResponse { recompile = 1; };
-            class setupInteractions { recompile = 1; };
-            class notorietyDecay { recompile = 1; }; // Om vi separerar den
+            class initGlobals {};
+            class scanZones {};
+            class economy {};
+            class zoneCapture {};
+            class notorietyResponse {};
+            class setupInteractions {};
         };
         class UI {
             file = "Missionframework\ui";
-            class initUI { recompile = 1; };
-            class updateResourceUI { recompile = 1; };
+            class initUI {};
+            class updateResourceUI {};
         };
         class Missions {
             file = "Missionframework\missions";
-            class generateInitialMission { recompile = 1; };
+            class generateInitialMission {};
         };
     };
 };
