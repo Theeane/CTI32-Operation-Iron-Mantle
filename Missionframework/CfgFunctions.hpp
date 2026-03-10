b033ea0e25836377825126e3c0fe050967d30159
Theeane <envall.kim@gmail.com>
2026-03-10 20:53:33 +0100

    Update CfgFunctions.hpp

--- Missionframework/CfgFunctions.hpp
+++ Missionframework/CfgFunctions.hpp
@@ -1,20 +1,28 @@
 class CfgFunctions {
     class AGS {
         tag = "AGS";
+        
         class Core {
             file = "Missionframework\core";
             class initGlobals {};
             class scanZones {};
             class economy {};
             class zoneCapture {};
-            class notorietyResponse {};
             class setupInteractions {};
+            class setupFOBInteractions {}; // For the FOB terminal
+            class setupFOBAction {};       // For the FOB container
+            class startFOBPlacement {};    // The Ghost Preview
+            class spawnFOBComposition {};  // The Global Spawning
+            class openBaseArchitect {};    // Zeus Mode
         };
+
         class UI {
             file = "Missionframework\ui";
             class initUI {};
             class updateResourceUI {};
+            class openBuildMenu {};        // The Build Menu UI
         };
+
         class Missions {
             file = "Missionframework\missions";
             class generateInitialMission {};
