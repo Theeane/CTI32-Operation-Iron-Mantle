260e4560387a5f4001175bde773cfaf0d805bfe9
Theeane <envall.kim@gmail.com>
2026-03-11 19:56:31 +0100

    Update CfgFunctions.hpp

--- Missionframework/CfgFunctions.hpp
+++ Missionframework/CfgFunctions.hpp
@@ -12,42 +12,44 @@ class CfgFunctions {
             file = "Missionframework\core";
             class initGlobals {};
             class initSystems {};
-            class economy {};
+            class initZones {};          // NY: Startar upp zonerna vid mission start
+            class economy {};            // UPPDATERAD: Hanterar inkomst & synk med butik
             class scanZones {};
-            class zoneCapture {};
+            class zoneCapture {};        // UPPDATERAD: Ger bonus vid capture
             class zoneManager {};
-            class setupInteractions {};
-            class setupFOBAction {};
-            class spawnFOBComposition {};
-            class startFOBPlacement {};
-            class startBuildPlacement {};
-            class finalizeBuild {};
-            class openBaseArchitect {};
-            class limitZeusAssets {};
             class persistence {};
             class undercoverHandler {};
-            class buildMode {};
+            class setupInteractions {};
+            class limitZeusAssets {};
         };
 
-        // --- NEW: BASE & LOGISTICS ---
+        // --- BASE, BUILDING & LOGISTICS ---
         class Base {
             file = "Missionframework\functions\base";
             class initFOB {};
             class packFOB {};
             class executeRepack {};
             class commanderToggleRepack {};
-            class initCommandPC {};
-            class updateBuyCategory {};  // Logic for the Buy Menu list
-            class initiatePurchase {};   // Logic for buying units/vehicles
+            class initCommandPC {};      // UPPDATERAD: Inkluderar nu Buy Menu & Strategic Maps
+            class updateBuyCategory {};  // NY: Logik för att fylla listan i butiken
+            class initiatePurchase {};   // NY: Köp-knappen med Spawn Pad-logik
+            class setupFOBAction {};
+            class spawnFOBComposition {};
+            class startFOBPlacement {};
+            class startBuildPlacement {};
+            class finalizeBuild {};
+            class openBaseArchitect {};
+            class buildMode {};
         };
 
         class UI {
             file = "Missionframework\ui";
             class initUI {};
-            class updateResourceUI {};
+            class updateResourceUI {};   // NY: Loopen som uppdaterar din HUD/Resource Bar
             class openBuildMenu {};
-            class openBuyMenu {};        // Opens our new Grey/English menu
-            class openCommandMap {};     // The Commander's overview map
+            class openBuyMenu {};        // NY: Öppnar din nya gråa butiks-meny
+            class openCommandMap {};
+            class openOpsMap {};         // NY: För din strategiska karta
         };
 
         class Missions {
