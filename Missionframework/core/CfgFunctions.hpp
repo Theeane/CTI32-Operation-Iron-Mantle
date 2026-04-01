920cea7daab0cee03f2a73f0c7178278e3232159
Theane <envall.kim@gmail.com>
2026-04-01 16:42:46 +0200

    Add files via upload

--- Missionframework/core/CfgFunctions.hpp
+++ Missionframework/core/CfgFunctions.hpp
@@ -1,16 +1,18 @@
 class CoreRuntime {
     file = "core";
 
-    class economy                { file = "core\MWF_fn_economy.sqf"; };
-    class enterBuildMode         { file = "core\MWF_fn_enterBuildMode.sqf"; };
-    class initGlobals            { file = "core\MWF_fn_initGlobals.sqf"; };
-    class initSystems            { file = "core\MWF_fn_initSystems.sqf"; };
-    class initZones              { file = "core\MWF_fn_initZones.sqf"; };
-    class limitZeusAssets        { file = "core\MWF_fn_limitZeusAssets.sqf"; };
-    class openBaseArchitect      { file = "core\MWF_fn_openBaseArchitect.sqf"; };
-    class presetManager          { file = "core\MWF_fn_presetManager.sqf"; };
-    class startBuildPlacement    { file = "core\MWF_fn_startBuildPlacement.sqf"; };
-    class syncEconomyState       { file = "core\MWF_fn_syncEconomyState.sqf"; };
-    class zoneCapture            { file = "core\MWF_fn_zoneCapture.sqf"; };
-    class zoneManager            { file = "core\MWF_fn_zoneManager.sqf"; };
+    class addResource             { file = "core\MWF_fn_addResource.sqf"; };
+    class economy                 { file = "core\MWF_fn_economy.sqf"; };
+    class enterBuildMode          { file = "core\MWF_fn_enterBuildMode.sqf"; };
+    class initGlobals             { file = "core\MWF_fn_initGlobals.sqf"; };
+    class initSystems             { file = "core\MWF_fn_initSystems.sqf"; };
+    class initZones               { file = "core\MWF_fn_initZones.sqf"; };
+    class limitZeusAssets         { file = "core\MWF_fn_limitZeusAssets.sqf"; };
+    class openBaseArchitect       { file = "core\MWF_fn_openBaseArchitect.sqf"; };
+    class presetManager           { file = "core\MWF_fn_presetManager.sqf"; };
+    class showNotification        { file = "core\MWF_fn_showNotification.sqf"; };
+    class startBuildPlacement     { file = "core\MWF_fn_startBuildPlacement.sqf"; };
+    class syncEconomyState        { file = "core\MWF_fn_syncEconomyState.sqf"; };
+    class zoneCapture             { file = "core\MWF_fn_zoneCapture.sqf"; };
+    class zoneManager             { file = "core\MWF_fn_zoneManager.sqf"; };
 };
